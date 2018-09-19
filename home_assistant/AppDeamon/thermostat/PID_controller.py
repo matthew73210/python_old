92c2180e7fb91a5aa04fe15ef507fb0391a4b467
Matthew Burton <matthewmaxwellburton@gmail.com>
2018-09-19 18:46:24 +0200

    Update PID_controller.py

--- home_assistant/AppDeamon/thermostat/PID_controller.py
+++ home_assistant/AppDeamon/thermostat/PID_controller.py
@@ -3,19 +3,26 @@
 #based on : https://github.com/ivmech/ivPID/blob/master/PID.py
 #and https://studentnet.cs.manchester.ac.uk/resources/library/thesis_abstracts/MSc14/FullText/Ioannidis-Feidias-fulltext.pdf
 
-#import appdaemon.plugins.hass.hassapi as hass
-import time
+import appdaemon.plugins.hass.hassapi as hass
+from datetime import datetime, timedelta
 from math import *
+import time
 
 
-class PID(): #add hass.Hass when in appdeamon
+class PID(hass.Hass): #add hass.Hass when in appdeamon
     
     def initialize(self): #This is where the variables used by the program will be set, and pulled when restarted.
         
         
-        self.kd = self.get_state("input_number.pid_kd", attribute="state")
-        self.ki = self.get_state("input_number.pid_ki", attribute="state")
-        self.kp = self.get_state("input_number.pid_kp", attribute="state")
+        self.kd = 0
+        self.ki = 0
+        self.kp = 0
+        
+        self.Pterm = 0
+        self.Iterm = 0
+        self.Dterm = 0
+        
+        self.output = 0
         
         self.setpoint = 0
         self.outside_temp = 0
@@ -23,6 +30,98 @@ class PID(): #add hass.Hass when in appdeamon
         self.error = 0
         self.last_error = 0
         
+        self.dt = 0
+        self.d_error = 0
+        
+        self.windup_guard = 20.0
+        
+        self.current_time = time.time()
+        self.last_time = self.current_time
+        
+        self.log("initialized")
+        
         #add function to check if pid values need calibrating. i.e. been reset to 0 or model is drifting to far from simulation
+        
+        self.run_every(self.test_function,datetime.now(),60)
+        
+        
+    def test_function(self,kwargs):
+    
+        self.kd = self.get_state("input_number.pid_kd", attribute="state")
+        self.ki = self.get_state("input_number.pid_ki", attribute="state")
+        self.kp = self.get_state("input_number.pid_kp", attribute="state")
+        
+        
+        self.setpoint = self.get_state("input_number.room_temp", attribute="state")
+        self.outside_temp = self.get_state("sensor.temp_outside", attribute="state")
+        self.room_temp = self.get_state("sensor.temp_lvr", attribute="state")
+        self.current_time = time.time()
+        
+        
+        self.error = float(self.setpoint) - float(self.room_temp)
+        
+        self.dt = self.current_time - self.last_time
+        self.d_error = self.error - self.last_error
+        
+        self.log("setpoint")
+        self.log(self.setpoint)
+        self.log("outside temp")
+        self.log(self.outside_temp)
+        self.log("room temp")
+        self.log(self.room_temp)
+        self.log("error")
+        self.log(self.error)
+        
+        self.log("kd,kp,kd")
+        self.log(self.kd)
+        self.log(self.kp)
+        self.log(self.ki)
+        
+        self.log("dt, d_error")
+        self.log(self.dt)
+        self.log(self.d_error)
+        
+        self.log("time, last_time")
+        self.log(self.current_time)
+        self.log(self.last_time)
+        
+        
+        self.last_time = self.current_time
+        self.last_error = self.error
+        
+        self.log("variables printed")
+        
+        if (self.Iterm < -self.windup_guard):
+            self.Iterm = -self.windup_guard
+            self.log("min guard set")
+        if (self.Iterm > self.windup_guard):
+            self.Iterm = self.windup_guard
+            self.log("max guard set")
+        
+        self.Pterm = float(self.kp)*float(self.error)
+        self.Iterm += float(self.ki)*(float(self.error)*float(self.dt))
+        self.Dterm = float(self.kd)*(float(self.d_error)/float(self.dt))
+        
+        self.output = self.Pterm + self.Dterm + self.Iterm
+        
+        self.log('pid terms')
+        self.log(self.Pterm)
+        self.log(self.Iterm)
+        self.log(self.Dterm)
+        
+        self.set_state("sensor.Pterm", state = self.Pterm)
+        self.set_state("sensor.Iterm", state = self.Iterm)
+        self.set_state("sensor.Dterm", state = self.Dterm)
+        self.set_state("sensor.output", state = self.output)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
