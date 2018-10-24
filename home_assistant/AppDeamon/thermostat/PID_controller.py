cc0ee10ef40651db70c591fb22c5577eee70c4f4
Matthew Burton <matthewmaxwellburton@gmail.com>
2018-10-24 09:32:42 +0200

    Update PID_controller.py
    Changed antiwind up to a non clipping on. It saturates and feeds this back into the integral to reduce its size.
    Reduced dt by a factor of 10 because it was getting too big and would mess with error later on.

--- home_assistant/AppDeamon/thermostat/PID_controller.py
+++ home_assistant/AppDeamon/thermostat/PID_controller.py
@@ -14,11 +14,13 @@ class PID(hass.Hass): #add hass.Hass when in appdeamon
     
     def initialize(self): #This is where the variables used by the program will be set, and pulled when restarted.
         
-        self.log_level = 0 #set to 0 to supress logs in functions
+        self.log_level = 1 #set to 0 to supress logs in functions
         
         self.kd = 0
         self.ki = 0
         self.kp = 0
+        
+        self.anti_windup = 0
         
         self.Pterm = 0
         self.Iterm = 0
@@ -33,6 +35,8 @@ class PID(hass.Hass): #add hass.Hass when in appdeamon
         self.error = 0
         self.last_error = 0
         self.heatloss = 0
+        self.windup = 0
+        self.windup_X = 0
         
         self.dt = 0
         self.d_error = 0
@@ -43,14 +47,13 @@ class PID(hass.Hass): #add hass.Hass when in appdeamon
         self.boiler_water_temp = 0
         self.temp_window = 1.5
         
-        self.current_time = time.time()
+        self.current_time = time.time()/(10) #had a problem where the integral would have a massive delta t, a consequence of have a 20 second cycle. Check dt with log that it is larger than 1 but no mre than 10. Otherwise it will osscilate with anti windup
         self.last_time = self.current_time
         
         self.log("initialized")
         
-        #add function to check if pid values need calibrating. i.e. been reset to 0 or model is drifting to far from simulation
         
-        self.run_every(self.mode_check,datetime.now(),60)
+        self.run_every(self.mode_check,datetime.now(),20)
         
     def mode_check(self,kwargs):
         
@@ -72,7 +75,28 @@ class PID(hass.Hass): #add hass.Hass when in appdeamon
         else:
             if self.log_level > 0:
                 self.log("Heating off")
-        
+            self.boiler_water_temp = 0
+            self.Pterm = 0
+            self.Iterm = 0
+            self.Dterm = 0
+            self.error = 0
+            self.output = 0
+            self.windup_X = 0
+            
+            
+        self.sendtoHA()
+            
+    def sendtoHA(self):
+        self.set_state("input_number.command_setpoint", state = self.boiler_water_temp )
+        self.set_state("sensor.Pterm", state = self.Pterm)
+        self.set_state("sensor.Iterm", state = self.Iterm)
+        self.set_state("sensor.Dterm", state = self.Dterm)
+        self.set_state("sensor.heating_error", state = self.error)
+        self.set_state("sensor.heating_output", state = self.output)
+        self.set_state("sensor.boiler_water_temp", state = self.boiler_water_temp)
+        self.set_state("sensor.windup_X", state = self.windup_X)
+        if self.log_level > 0:
+                self.log("Command sent to HA")
         
         
     def Hysteresis(self,kwargs):
@@ -90,70 +114,69 @@ class PID(hass.Hass): #add hass.Hass when in appdeamon
         
     def PID(self,kwargs):
         
+        #reset main windup variable
+        self.windup = 0
+        
         #Pull PID coeffs from HA
         self.kd = self.get_state("input_number.pid_kd", attribute="state")
         self.ki = self.get_state("input_number.pid_ki", attribute="state")
         self.kp = self.get_state("input_number.pid_kp", attribute="state")
-        
-        #Pull heatloss coeff from HA
-        self.insulation_coeff = self.get_state("input_number.insulation_coeff", attribute="state")
+        self.anti_windup = self.get_state("input_number.pid_anti_windup", attribute="state")
         
         #Pull temps and setpoint from HA
         self.setpoint = self.get_state("input_number.room_temp", attribute="state")
-        self.outside_temp = self.get_state("sensor.temp_outside", attribute="state")
         self.room_temp = self.get_state("sensor.temp_lvr", attribute="state")
         
         #Get time
-        self.current_time = time.time()
-        
-        #Heatloss calculation, as load disturbance 
-        self.heatloss = float(self.insulation_coeff)*(float(self.room_temp)-float(self.outside_temp))
-        
+        self.current_time = time.time()/(10)
+        self.log("time")
+        self.log(self.current_time)
+
         #Calculate dt, error and d_error
         self.dt = self.current_time - self.last_time
-        self.error = float(self.setpoint) - float(self.room_temp) + self.heatloss
+        self.log("dt")
+        self.log(self.dt)
+        self.error = float(self.setpoint) - float(self.room_temp)
         self.d_error = self.error - self.last_error
+        
+        if self.error < 0:
+            self.error = 0
         
         
         #Push values to memory for next iteration
         self.last_time = self.current_time
         self.last_error = self.error
         
-        #Input heatloss into error
-        self.heatloss = float(self.insulation_coeff)*(float(self.room_temp)-float(self.outside_temp))
-        
         #Calculate PID
         self.Pterm = float(self.kp)*float(self.error)
-        self.Iterm += float(self.ki)*(float(self.error)*float(self.dt))
+        self.Iterm += float(self.ki)*((float(self.error) + float(self.windup_X))*float(self.dt))
         self.Dterm = float(self.kd)*(float(self.d_error)/float(self.dt))
         
         self.output = self.Pterm + self.Dterm + self.Iterm
         
         #Clip output and Iterm agaisnt windup
         if self.output > self.max_output:
+            self.windup = -float(self.output) + float(self.max_output)
+            
             self.output = self.max_output
-            self.Iterm = self.windup_guard
-            self.log("output and Iterm clipped")
+            self.log("outputclipped")
         if self.output < -self.max_output:
-            self.output = -self.max_output
-            self.Iterm = -self.windup_guard
-            self.log("output and Iterm clipped")
+            self.output = 0
+            self.log("output clipped")
+
+        
+        self.windup_X = float(self.windup) * float(self.anti_windup)
+        
+        self.log("windup and windup X")
+        self.log(self.windup)
+        self.log(self.windup_X)
+        
+        
         
         #Temp mapping
         self.boiler_water_temp = (self.output*self.max_boiler_water_temp)/self.max_output
         
         if self.boiler_water_temp < 10:
             self.boiler_water_temp = 0
+
         
-        
-        
-        
-        
-        self.set_state("sensor.Pterm", state = self.Pterm)
-        self.set_state("sensor.Iterm", state = self.Iterm)
-        self.set_state("sensor.Dterm", state = self.Dterm)
-        self.set_state("sensor.heatloss", state = self.heatloss)
-        self.set_state("sensor.heating_error", state = self.error)
-        self.set_state("sensor.heating_output", state = self.output)
-        self.set_state("sensor.boiler_water_temp", state = self.boiler_water_temp)
-      
